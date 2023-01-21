3296dcd034a1309fc5759237249a8ee63f8e1b79
roy2pushkar <roy2000pushkar@gmail.com>
2023-01-22 00:12:43 +0530

    day5

---
+++ Day5/productOfArrayExceptSelf.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+       int n=nums.size();
+        vector<int>res(n);
+        res[0]=1;
+        for(int i=1;i<n;i++)
+        {
+            res[i]=res[i-1] * nums[i-1];
+        }
+        int right=1;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(i < n-1)
+            {
+                right=right * nums[i+1];
+            }
+            res[i]=res[i] * right;
+        }
+        return res;
+    }
+};
