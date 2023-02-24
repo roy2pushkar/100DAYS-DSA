0178a92af8e152add01c2b94c91887797592b981
roy2pushkar <roy2000pushkar@gmail.com>
2023-02-24 22:34:02 +0530

    day 36

---
+++ Day36/product_Array_puzzle.cpp
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+    vector<long long int> productExceptSelf(vector<long long int>& arr, int n) {
+        //code here
+        vector <long long int> v;
+        int count=0;
+        long long int prod=1;
+        bool flag =true;
+
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+            {
+                flag=false;
+                count++;
+
+            }
+            else
+            {
+               prod=prod*arr[i];
+            }
+        }
+
+        if(!flag)
+        {
+            for(int i=0;i<n;i++)
+        {
+            if(arr[i]==0 && count==1)
+               v.push_back(prod);
+
+            else if(arr[i]==0 && count > 1)
+                v.push_back(0);
+            else
+               v.push_back(0);
+
+        }
+    }
+
+    else
+    {
+         for(int i=0;i<n;i++)
+            {
+                v.push_back(prod/arr[i]);
+            }
+    }
+
+        return v;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;  // number of test cases
+    cin>>t;
+    while(t--)
+    {
+        int n;  // size of the array
+        cin>>n;
+        vector<long long int> arr(n),vec(n);
+
+        for(int i=0;i<n;i++)    // input the array
+        {
+            cin>>arr[i];
+        }
+        Solution obj;
+        vec = obj.productExceptSelf(arr,n);   // function call
+
+        for(int i=0;i<n;i++)    // print the output
+        {
+            cout << vec[i] << " ";
+        }
+        cout<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
